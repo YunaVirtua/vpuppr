f56064a29f206cf11f7acfb5ac69f0591cd41a67
Yuna Virtua <32870735+YunaVirtua@users.noreply.github.com>
2024-05-02 15:33:24 +0200

    Update setup.sh
    Made functional on macOS

--- setup.sh
+++ setup.sh
@@ -1,12 +1,8 @@
-#!/bin/sh
-
-cd $(dirname "$0")
-
 echo "Updating git submodules"
 git submodule update --init --recursive --remote
 
 echo "Building rust lib"
-python libvpuppr/build.py --debug
+python3 libvpuppr/build.py --debug
 
 echo "Copying gdextension files"
 cp libvpuppr/libvpuppr.gdextension .
